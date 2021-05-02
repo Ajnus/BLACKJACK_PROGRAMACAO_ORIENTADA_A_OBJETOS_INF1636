f3a7003ff78e11d1c17a0bdcaaefc7f25ba44d89
1PauloRod <70543693+1PauloRod@users.noreply.github.com>
2021-05-02 02:46:17 -0300

    Add files via upload

---
+++ 1713170/src/model/Dealer.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Dealer {
+	
+	private Mao mao = new Mao();
+	
+	public void adicionarCarta(Carta carta)
+	{
+		if (mao.getNumeroDeCartas() <= 17)
+			mao.adicionarCarta(carta);
+	}
+	
+	public void esvaziarMao()
+	{
+		mao.esvaziarMao();
+	}
+	
+}
