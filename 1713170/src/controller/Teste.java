d55d1dc0541427d8833e5e6eac51b8687ae106d1
Jam Ajna Soares <ajnasoares@hotmail.com>
2021-06-11 02:11:59 -0300

    clique fichas, + aposta, - saldo ok.

--- 1713170/src/controller/Teste.java
+++ 1713170/src/controller/Teste.java
@@ -3,13 +3,18 @@ package controller;
 import model.Jogador;
 
 public class Teste {
-	
+
 	public Teste() {
 	}
-	
+
 	public String exibeSaldo(Jogador jogador) {
 		String saldo = String.valueOf(jogador.getSaldo());
 		return saldo;
+	}
+
+	public String exibeAposta(Jogador jogador) {
+		String aposta = String.valueOf(jogador.getAposta());
+		return aposta;
 	}
 
 }
