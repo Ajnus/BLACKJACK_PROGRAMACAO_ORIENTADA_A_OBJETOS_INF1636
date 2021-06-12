7b5b06091cbe24bb5a6c696e821e7f2457cb23ca
1PauloRod <70543693+1PauloRod@users.noreply.github.com>
2021-06-12 04:35:43 -0300

    Create TelaJogadorPanel.java

---
+++ 1713170/src/view/TelaJogadorPanel.java
@@ -0,0 +1,34 @@
+package view;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.JPanel;
+
+public class TelaJogadorPanel extends JPanel{
+
+	private static final long serialVersionUID = 1L;
+	int LARGURA = 900;
+	int ALTURA = 700;
+	private Image imagemBackGround = Toolkit.getDefaultToolkit().getImage("C:\\Users\\paulo\\INF1636\\Imagens\\blackjack.png");
+	Graphics g;
+	
+	public TelaJogadorPanel()
+	{
+		setLayout(null);
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		try {
+			g.drawImage(imagemBackGround, 0, 0, this);
+			g.setColor(new Color(17, 68, 26));
+			g.fillRect(5, 520, 170, 550);
+
+		} catch (Exception e) {
+			System.err.println("Imagem não encontrada!");
+		}
+	}
+}
