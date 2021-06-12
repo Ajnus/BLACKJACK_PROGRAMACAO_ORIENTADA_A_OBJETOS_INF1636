80b766558ccc31b910b0c309764175c7ac59131d
1PauloRod <70543693+1PauloRod@users.noreply.github.com>
2021-06-12 04:33:47 -0300

    Update TelaInicio.java

--- 1713170/src/view/TelaInicio.java
+++ 1713170/src/view/TelaInicio.java
@@ -1,28 +1,46 @@
 package view;
 
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class TelaInicio extends JPanel implements ActionListener {
+import controller.BotaoNumeroJogador;
+import controller.GameController;
+
+import java.awt.*;
+
+
+public class TelaInicio extends JFrame{
 
 	private static final long serialVersionUID = 1L;
-
-	private boolean stateIniciar = true;
+	
+	//Ajustes de tela
+	Toolkit tk = Toolkit.getDefaultToolkit();
+	Dimension screenSize = tk.getScreenSize();
+	int screenWidth = screenSize.width;
+	int screenHeight = screenSize.height;
+	JPanel panel = new JPanel();
+	
 	private JButton[] jogadores = new JButton[4];
 
-	public TelaInicio(int LARGURA, int ALTURA) {
-		setLayout(null);
-		setBackground(new Color(5, 83, 22));
+	public TelaInicio(GameController game, int LARGURA, int ALTURA) {
+		
+		//Configuração JFrame
+		setTitle("BlackJack");
+		setResizable(false);
+		int x = screenWidth / 2 - LARGURA / 2;
+		int y = screenHeight / 2 - ALTURA / 2;
+		setBounds(x, y, LARGURA, ALTURA);
+		
+		//Configuração JPanel
+		panel.setLayout(null);
+		panel.setBackground(new Color(5, 83, 22));
 		JLabel titleLabel = new JLabel("BlackJack");
 		titleLabel.setFont(new Font("TimesRoman", Font.PLAIN, 50));
 		titleLabel.setForeground(new Color(200, 200, 200));
 		titleLabel.setBounds(LARGURA / 2 - 120, -300, LARGURA, ALTURA);
-		add(titleLabel);
+		panel.add(titleLabel);
 
 		for (int i = 0; i < 4; i++) {
 			if (i == 0) {
@@ -34,14 +52,8 @@ public class TelaInicio extends JPanel implements ActionListener {
 				jogadores[i].setFocusPainted(false);
 				jogadores[i].setOpaque(false);
 				jogadores[i].setBounds(LARGURA / 2 - 140, (ALTURA / 2 - 50) + 50 * i, 200, 30);
-				jogadores[i].addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) { // jogar futuramente essa funcionalidade para o
-																	// controller para poder pegar a quantidade de
-																	// jogadores.
-						stateIniciar = false;
-					}
-				});
-				add(jogadores[i]);
+				jogadores[i].addActionListener(new BotaoNumeroJogador(i+1, this));
+				panel.add(jogadores[i]);
 			} else {
 				jogadores[i] = new JButton(String.format("%d jogadores", i + 1));
 				jogadores[i].setFont(new Font("TimesRoman", Font.PLAIN, 25));
@@ -51,24 +63,11 @@ public class TelaInicio extends JPanel implements ActionListener {
 				jogadores[i].setFocusPainted(false);
 				jogadores[i].setOpaque(false);
 				jogadores[i].setBounds(LARGURA / 2 - 130, (ALTURA / 2 - 50) + 50 * i, 200, 30);
-				jogadores[i].addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) { // jogar futuramente essa funcionalidade para o
-																	// controller para poder pegar a quantidade de
-																	// jogadores.
-						stateIniciar = false;
-					}
-				});
-				add(jogadores[i]);
+				jogadores[i].addActionListener(new BotaoNumeroJogador(i+1, this));
+				panel.add(jogadores[i]);
 			}
 		}
+		add(panel);
 	}
-
-	public boolean getStateIniciar() {
-		return stateIniciar;
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-	}
-
+	
 }
