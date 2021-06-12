af6bcd0543e550302d3e2abe29f86192e7e1c6db
Jam Ajna Soares <ajnasoares@hotmail.com>
2021-06-11 21:36:27 -0300

    clique fichas, + aposta, - saldo ok.

--- 1713170/src/view/Game.java
+++ 1713170/src/view/Game.java
@@ -1,6 +1,8 @@
 package view;
 
-import java.util.Scanner;
+import java.awt.Point;
+
+//import java.util.Scanner;
 
 import model.Jogador;
 
@@ -9,21 +11,21 @@ public class Game {
 	private final int ALTURA = 700;
 
 	public Game() {
-		Jogador[] jogadores;
-		Scanner n = new Scanner(System.in);
-		String nomes;
+		Jogador[] jogadores = new Jogador[4];
+		// Scanner n = new Scanner(System.in);
+		String[] nomes = { "Jam", "Luan", "Paulo", "GBG" };
 		TelaInicio telaInicio = new TelaInicio(LARGURA, ALTURA);
 		Window windowInicio = new Window("BlackJack", LARGURA, ALTURA);
 		windowInicio.getContentPane().add(telaInicio);
 		boolean ligado = true;
-		
-		jogadores = new Jogador[4];
+
+		// jogadores = ;
 
 		for (int i = 0; i < 4; i++) {
 			System.out.println("Nome do jogador " + (i + 1) + ':');
-			nomes = n.next();
+			// nomes = n.next();
 			jogadores[i] = new Jogador(i + 1);
-			jogadores[i].setNome(nomes);
+			jogadores[i].setNome(nomes[i]);
 		}
 
 		for (int i = 0; i < 4; i++) {
@@ -31,7 +33,7 @@ public class Game {
 			System.out.println("Codigo: " + jogadores[i].getId());
 			System.out.println("Saldo: " + jogadores[i].getSaldo());
 		}
-		
+
 		while (ligado) {
 			System.out.println("");
 			if (telaInicio.getStateIniciar() == false) {
@@ -43,12 +45,83 @@ public class Game {
 
 		if (telaInicio.getStateIniciar() == false) {
 			TelaJogador telaJogador = new TelaJogador(LARGURA, ALTURA, jogadores);
+			telaJogador.addMouseListener(new java.awt.event.MouseAdapter() {
+				public void mouseClicked(java.awt.event.MouseEvent evt) {
+					System.out.println("mouseClicked");
+					Point p = evt.getPoint();
+					System.out.printf("X: %d, Y: %d\n", p.x, p.y);
+
+					// FICHAS
+					if ((p.x >= 752 && p.x <= 787) && (p.y >= 597 && p.y <= 627)) {
+						System.out.println("FICHA 1");
+						//if (jogadores[0].verificarAposta())
+							jogadores[0].adicionarAposta(1);
+						System.out.printf("Valor aposta: %d\n", jogadores[0].getAposta());
+						System.out.printf("Valor saldo: %d\n", jogadores[0].getSaldo());
+						telaJogador.apostaLabel.setText(String.valueOf(jogadores[0].getAposta()));
+						telaJogador.saldoLabel.setText(String.valueOf(jogadores[0].getSaldo()));
+					}
+
+					else if ((p.x >= 797 && p.x <= 828) && (p.y >= 597 && p.y <= 627)) {
+						System.out.println("FICHA 5");
+						//if (jogadores[0].verificarAposta())
+							jogadores[0].adicionarAposta(5);
+						System.out.printf("Valor aposta: %d\n", jogadores[0].getAposta());
+						System.out.printf("Valor saldo: %d\n", jogadores[0].getSaldo());
+						telaJogador.apostaLabel.setText(String.valueOf(jogadores[0].getAposta()));
+						telaJogador.saldoLabel.setText(String.valueOf(jogadores[0].getSaldo()));
+					}
+
+					else if ((p.x >= 837 && p.x <= 870) && (p.y >= 597 && p.y <= 627)) {
+						System.out.println("FICHA 10");
+						//if (jogadores[0].verificarAposta())
+							jogadores[0].adicionarAposta(10);
+						System.out.printf("Valor aposta: %d\n", jogadores[0].getAposta());
+						System.out.printf("Valor saldo: %d\n", jogadores[0].getSaldo());
+						telaJogador.apostaLabel.setText(String.valueOf(jogadores[0].getAposta()));
+						telaJogador.saldoLabel.setText(String.valueOf(jogadores[0].getSaldo()));
+					}
+
+					else if ((p.x >= 750 && p.x <= 782) && (p.y >= 633 && p.y <= LARGURA)) {
+						System.out.println("FICHA 20");
+						//if (jogadores[0].verificarAposta())
+							jogadores[0].adicionarAposta(20);
+						System.out.printf("Valor aposta: %d\n", jogadores[0].getAposta());
+						System.out.printf("Valor saldo: %d\n", jogadores[0].getSaldo());
+						telaJogador.apostaLabel.setText(String.valueOf(jogadores[0].getAposta()));
+						telaJogador.saldoLabel.setText(String.valueOf(jogadores[0].getSaldo()));
+					}
+
+					else if ((p.x >= 797 && p.x <= 828) && (p.y >= 633 && p.y <= LARGURA)) {
+						System.out.println("FICHA 50");
+						//if (jogadores[0].verificarAposta())
+							jogadores[0].adicionarAposta(50);
+						System.out.printf("Valor aposta: %d\n", jogadores[0].getAposta());
+						System.out.printf("Valor saldo: %d\n", jogadores[0].getSaldo());
+						telaJogador.apostaLabel.setText(String.valueOf(jogadores[0].getAposta()));
+						telaJogador.saldoLabel.setText(String.valueOf(jogadores[0].getSaldo()));
+					}
+
+					else if ((p.x >= 844 && p.x <= 877) && (p.y >= 633 && p.y <= LARGURA)) {
+						System.out.println("FICHA 100");
+						//if (jogadores[0].verificarAposta())
+							jogadores[0].adicionarAposta(100);
+						System.out.printf("Valor aposta: %d\n", jogadores[0].getAposta());
+						System.out.printf("Valor saldo: %d\n", jogadores[0].getSaldo());
+						telaJogador.apostaLabel.setText(String.valueOf(jogadores[0].getAposta()));
+						telaJogador.saldoLabel.setText(String.valueOf(jogadores[0].getSaldo()));
+					}
+				}
+			});
+
 			Window windowJogador = new Window("BlackJack", LARGURA, ALTURA);
 			windowJogador.getContentPane().add(telaJogador);
 
-			TelaDealer telaDealer = new TelaDealer(LARGURA, ALTURA);
-			Window windowDealer = new Window("BlackJack", LARGURA, ALTURA);
-			windowDealer.getContentPane().add(telaDealer);
+			/*
+			 * TelaDealer telaDealer = new TelaDealer(LARGURA, ALTURA); Window windowDealer
+			 * = new Window("BlackJack", LARGURA, ALTURA);
+			 * windowDealer.getContentPane().add(telaDealer);
+			 */
 		}
 	}
 }
