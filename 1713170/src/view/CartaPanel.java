ac7887ad9d0b2214108817c027b86d891295b0ea
1PauloRod <70543693+1PauloRod@users.noreply.github.com>
2021-06-12 04:32:27 -0300

    Create CartaPanel.java

---
+++ 1713170/src/view/CartaPanel.java
@@ -0,0 +1,53 @@
+package view;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+public class CartaPanel extends JPanel{
+ static final long serialVersionUID = 1L;
+	public ArrayList<Image> imagemCartas = new ArrayList<Image>();
+	
+	
+	public void desenhaCarta(String carta)
+	{
+		Image imagemCarta = null;
+		
+		try {
+			imagemCarta = Toolkit.getDefaultToolkit().getImage("C:\\Users\\paulo\\INF1636\\Imagens\\"+carta+".png");;
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+			System.exit(1);
+		}
+		
+		imagemCartas.add(imagemCarta);
+		repaint();
+		
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		int panelSize = getSize().width;
+		int posX = -50;
+		int posY = 180;
+		
+		for(Image img: imagemCartas) {
+			if (panelSize < posX + img.getWidth(null)){
+				posX = 10;
+				posY += 60;
+			}
+			else {
+				posX += 60;
+			}
+			
+			g.drawImage(img, posX, posY, 50, 50, null);
+		}
+	}
+	
+	
+}
